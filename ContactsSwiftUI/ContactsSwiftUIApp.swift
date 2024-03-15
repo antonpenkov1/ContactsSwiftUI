fdb5aba7aa1fc4a9d05942a652b149ee8b57475e
anton_penkov <anton.penkov1@gmail.com>
2024-03-15 15:36:25 +0100

    Initial Commit

---
+++ ContactsSwiftUI/ContactsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSwiftUIApp.swift
+//  ContactsSwiftUI
+//
+//  Created by Антон Пеньков on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
